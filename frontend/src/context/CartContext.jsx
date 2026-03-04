450069842e1bf00600cd6d1ebe45c888fff97d0e
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-03-04 01:15:40 +0000

    Creación de contexto de carrito de compras CartContext.jsx y añadido al main.jsx

---
+++ frontend/src/context/CartContext.jsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useState, useEffect} from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [ cart, setCart ] = useState( () => {
+    try {
+      const savedCart = localStorage.getItem('cart');
+      const parsedCart = JSON.parse(savedCart);
+      return Array.isArray(parsedCart)? parsedCart : []
+    } catch (error) {
+      return []
+    }
+  })
+
+  const addToCart = (newItem) => {
+    setCart((prevCart) => {
+      let findItem = false;
+
+      const updatedCart = prevCart.map((item) => {
+        if (item.product_id === newItem.product_id) {
+
+          findItem = true;
+          return {
+            ...item,
+            quantity: (item.quantity + newItem.quantity)
+          }
+        }
+        else {
+          return item;
+        }
+      });
+
+      if (findItem) {
+        return updatedCart;
+      }
+      return [...prevCart, newItem];
+
+    });
+  };
+
+  const removeFromCart = (idToRemove) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.filter((item) => item.product_id !== idToRemove)
+      return updatedCart
+    });
+  };
+
+
+  const updateQuantity = (idToUpdate, newQuantity) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => {
+        if (item.product_id === idToUpdate) {
+          const validateQuantity = (newQuantity >=1)? newQuantity : 1
+          return {...item, quantity: validateQuantity};
+        }
+        return item;
+      })
+      return updatedCart;
+    });
+  };
+
+
+  const clearCart = () => {
+    setCart([]);
+  }
+    
+
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, updateQuantity, clearCart}}>
+      {children}
+    </CartContext.Provider>
+  )
+
+};
+
+export const useCart = () => useContext(CartContext)

95ca0b4bd0bf83b124a909107d0cbc219a11ec02
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-03-31 22:36:07 +0000

    Se agrega titulo a UserMyAccountPage y se ajusta su hoja de estilos

--- frontend/src/pages/user/UserMyAccountPage.jsx
+++ frontend/src/pages/user/UserMyAccountPage.jsx
@@ -29,28 +29,33 @@ function UserMainInfo() {
 
 
 	return(
-		<div className='user-info'>
-			{!user.is_profile_complete && <h3> Aún no has completado tu perfil, puedes completarlo en la sección "Actualizar Información"</h3> }
-			<p><strong>Email: </strong>{user.email}</p>
-			<p><strong>Nombre: </strong>{user.first_name? user.first_name : 'No indicado'}</p>
-			<p><strong>Apellido: </strong>{user.last_name? user.last_name : 'No indicado'}</p>
-			<p><strong>Teléfono: </strong>{user.phone? user.phone : 'No indicado'}</p>
-			<p><strong>Email verificado:</strong> {user.is_verified? <CheckCircleIcon className='verified-check'/> : <CircleXIconBg className='non-verified-check' /> } </p>
+		<div className='user-info-container'>
+			<h1>Información de la cuenta</h1>
+			<div className='user-info'>
+				{!user.is_profile_complete && <h3> Aún no has completado tu perfil, puedes completarlo en la sección "Actualizar Información"</h3> }
+				<p><strong>Email: </strong>{user.email}</p>
+				<p><strong>Nombre: </strong>{user.first_name? user.first_name : 'No indicado'}</p>
+				<p><strong>Apellido: </strong>{user.last_name? user.last_name : 'No indicado'}</p>
+				<p><strong>Teléfono: </strong>{user.phone? user.phone : 'No indicado'}</p>
+				<p><strong>Email verificado:</strong> {user.is_verified? <CheckCircleIcon className='verified-check'/> : <CircleXIconBg className='non-verified-check' /> } </p>
 
-			{!user.is_verified &&
-			<div className='verify-email-section'>
-				<button className='info-verify-btn' onClick={handleVerifyBtn} disabled={isActive}>Reenviar correo de verificación</button>
-				{isActive &&
-				<div className='verify-messages'>
-					<p>Revisa tu bandeja de correo (Recuerda revisar la carpeta de spam) </p>
-					<p>Puedes volver a intentar dentro de {Math.floor(timeLeft/60)} minutos y {timeLeft - ((Math.floor(timeLeft/60))* 60)} segundos</p>
-					{errors && <span>{errors}</span> }
-				</div> }
+				{!user.is_verified &&
+				<div className='verify-email-section'>
+					<button className='info-verify-btn' onClick={handleVerifyBtn} disabled={isActive}>Reenviar correo de verificación</button>
+					{isActive &&
+					<div className='verify-messages'>
+						<p>Revisa tu bandeja de correo (Recuerda revisar la carpeta de spam) </p>
+						<p>Puedes volver a intentar dentro de {Math.floor(timeLeft/60)} minutos y {timeLeft - ((Math.floor(timeLeft/60))* 60)} segundos</p>
+						{errors && <span>{errors}</span> }
+					</div> }
+				</div>
+				}
+				
+			
 			</div>
-			}
-			
-			
+
 		</div>
+
 	)
 
 }

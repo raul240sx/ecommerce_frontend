7ec6930a24b63d16f975ac99f4774efc2d41d072
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-03-31 23:02:30 +0000

    Se agrega titulo a UserOrdersPage y se ajusta su hoja de estilos

--- frontend/src/pages/user/UserOrdersPage.jsx
+++ frontend/src/pages/user/UserOrdersPage.jsx
@@ -10,7 +10,7 @@ import './UserOrdersPage.css'
 
 
 function UserOrders() {
-  const { user , checkAuth } = useAuth();
+  const { checkAuth } = useAuth();
   const { orders, loading, error , updateFilters, searchParams } = useOrders();
   const currentStatus = searchParams.get('status') || '';
   const currentOrdering = searchParams.get('ordering') || '';
@@ -20,71 +20,81 @@ function UserOrders() {
   },[])
 
   if (error) {
-    return ( <div>Error al cargar las órdenes</div> )
+    return( <div>Error al cargar las órdenes</div> )
   }
 
   if (loading) {
-    return ( <div>Cargando órdenes</div> )
+    return( <div>Cargando órdenes</div> )
   }
+
 
   return(
     <div className='orders-container'>
+      <h1>Órdenes de compra</h1>
       <div className='orders-content'>
-        <div className='orders-table-section'>
-          <table className='orders-table'>
-            <thead className='orders-table-titles'>
-              <tr>
-                <th>ID de orden</th>
-                <th>Monto</th>
-                <th>Estado</th>
-                <th>Fecha Creación</th>
-                <th>Ver</th>
-              </tr>
-            </thead>
-            <tbody className='orders-table-data'>
-              {orders?.map((order, index) => (
-                <tr key={index} className={`order-row ${index % 2 === 0 ? '' : 'odd-row'}`}>
-                  <td>{order.id}</td>
-                  <td>{formatter.format(order.total_amount)}</td>
-                  <td>{formatStatus(order.status) }</td>
-                  <td>{formatDate(order.created_date)}</td>
-                  <td> <Link to={`/order-detail/${order.id}`}><SearchIcon/></Link>  </td>
+        {orders.length <= 0? 
+          <div className='no-orders-container'>
+            <p>Aún no tienes órdenes de compra</p>
+          </div> :
+          <>
+          <div className='orders-table-section'>
+            <table className='orders-table'>
+              <thead className='orders-table-titles'>
+                <tr>
+                  <th>ID de orden</th>
+                  <th>Monto</th>
+                  <th>Estado</th>
+                  <th>Fecha Creación</th>
+                  <th>Ver</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className='table-filters'>
-          <div className='state-filter'>
-            <label htmlFor='items'>Filtar por estado:</label>
-            <select 
-            value={currentStatus}
-            onChange={(e) => updateFilters({ status: e.target.value})}>
-              <option value=''>Todos</option>
-              <option value='PAID'>PAGADOS</option>
-              <option value='PENDING'>PENDIENTES</option>
-              <option value='CANCELLED'>CANCELADOS</option>
-            </select>
+              </thead>
+              <tbody className='orders-table-data'>
+                {orders?.map((order, index) => (
+                  <tr key={index} className={`order-row ${index % 2 === 0 ? '' : 'odd-row'}`}>
+                    <td>{order.id}</td>
+                    <td>{formatter.format(order.total_amount)}</td>
+                    <td>{formatStatus(order.status) }</td>
+                    <td>{formatDate(order.created_date)}</td>
+                    <td> <Link to={`/order-detail/${order.id}`}><SearchIcon/></Link>  </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          
-          <div className='sorting-buttons'>
-            <span>Ordenar por monto:</span>
-            <div className='sorting-btn-container'>
-              <button 
-                className={`filter-btn ${currentOrdering === 'total_amount' ? 'active' : ''}`} onClick={() => updateFilters({ ordering: 'total_amount' })}>
-                Menor a Mayor
-              </button>
-              <button 
-                className={`filter-btn ${currentOrdering === '-total_amount' ? 'active' : ''}`} onClick={() => updateFilters({ ordering: '-total_amount' })}>
-                Mayor a Menor
-              </button>
-            </div> 
-          </div>
+          <div className='table-filters'>
+            <div className='state-filter'>
+              <label htmlFor='items'>Filtar por estado:</label>
+              <select 
+              value={currentStatus}
+              onChange={(e) => updateFilters({ status: e.target.value})}>
+                <option value=''>Todos</option>
+                <option value='PAID'>PAGADOS</option>
+                <option value='PENDING'>PENDIENTES</option>
+                <option value='CANCELLED'>CANCELADOS</option>
+              </select>
+            </div>
+            
+            <div className='sorting-buttons'>
+              <span>Ordenar por monto:</span>
+              <div className='sorting-btn-container'>
+                <button 
+                  className={`filter-btn ${currentOrdering === 'total_amount' ? 'active' : ''}`} onClick={() => updateFilters({ ordering: 'total_amount' })}>
+                  Menor a Mayor
+                </button>
+                <button 
+                  className={`filter-btn ${currentOrdering === '-total_amount' ? 'active' : ''}`} onClick={() => updateFilters({ ordering: '-total_amount' })}>
+                  Mayor a Menor
+                </button>
+              </div> 
+            </div>
 
-          <button className='filter-btn' onClick={() => updateFilters({ ordering: '', status: '', limit: '' })}>
-            Limpiar todos los filtros
-          </button>
-        </div>
+            <button className='filter-btn' onClick={() => updateFilters({ ordering: '', status: '', limit: '' })}>
+              Limpiar todos los filtros
+            </button>
+          </div>
+          </>
+        }
+        
       </div>
     </div>
   )

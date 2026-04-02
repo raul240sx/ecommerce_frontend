87665fd8f9e8d0b0e54d4e80637db9df24b9c5d1
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-04-02 00:31:41 +0000

    Se añaden estilos a las paginas AboutPage.jsx y LegalPage.jsx

--- frontend/src/pages/about/AboutPage.jsx
+++ frontend/src/pages/about/AboutPage.jsx
@@ -1,21 +1,30 @@
+import './AboutPage.css'
+
 
 function AboutPage() {
 
 
   return(
-		<div>
-			<p>Somos una tienda online especializada en guitarras eléctricas, amplificadores
-				y accesorios para guitarristas que buscan calidad, sonido y confianza en cada compra.</p>
-				<p>Nacimos con la convicción de que el equipo correcto no solo mejora la técnica,
-				sino que potencia la identidad musical de cada artista.</p>
-				<p>Seleccionamos cuidadosamente cada producto de nuestro catálogo, priorizando marcas reconocidas
-				y equipos con excelente relación calidad-precio, para que tanto principiantes como músicos
-				experimentados encuentren el instrumento y los accesorios ideales para su estilo.</p>
-				<p>Más que vender productos, buscamos acompañar a cada guitarrista en su crecimiento musical,
-				ofreciendo asesoría clara, información técnica precisa y una experiencia de compra simple,
-				segura y transparente.</p>
-				<p>Creemos que cada riff, cada solo y cada acorde cuentan una historia. Nuestra misión es 
-				ayudarte a encontrar el sonido que la represente.</p>
+		<div className='about-container'>
+			<div className='about-content'>
+				<h1>Acerca de Guitarzone.cl</h1>
+
+				<div className='about-info'>
+					<p>Somos una tienda online especializada en guitarras eléctricas, amplificadores
+					y accesorios para guitarristas que buscan calidad, sonido y confianza en cada compra.</p>
+					<p>Nacimos con la convicción de que el equipo correcto no solo mejora la técnica,
+					sino que potencia la identidad musical de cada artista.</p>
+					<p>Seleccionamos cuidadosamente cada producto de nuestro catálogo, priorizando marcas reconocidas
+					y equipos con excelente relación calidad-precio, para que tanto principiantes como músicos
+					experimentados encuentren el instrumento y los accesorios ideales para su estilo.</p>
+					<p>Más que vender productos, buscamos acompañar a cada guitarrista en su crecimiento musical,
+					ofreciendo asesoría clara, información técnica precisa y una experiencia de compra simple,
+					segura y transparente.</p>
+					<p>Creemos que cada riff, cada solo y cada acorde cuentan una historia. Nuestra misión es 
+					ayudarte a encontrar el sonido que la represente.</p>
+				</div>
+
+			</div>
 		</div>
 	)
 }
